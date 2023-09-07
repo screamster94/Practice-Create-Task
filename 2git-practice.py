49617480bc9ca8013739c83090e3358c10156853
Kareem Awad <screamster94@gmail.com>
2023-09-07 20:52:48 +0000

    canvas activity

---
+++ 2git-practice.py
@@ -0,0 +1,11 @@
+toDoList = ["Math Homework", "Cook Dinner", "Fold Laundry"]
+
+def addItem(item):
+   print(toDoList)
+   return toDoList
+
+userAns = input("Do you want to add to your list or quit? A/Q")
+while userAns == "A":
+   item = input("What item do you want to add?")
+   addItem(item)
+   userAns = input("Do you want to add to your list or quit? A/Q")
